dd136b851741fa0595a8d4286059c6a9ed606822
TeterinMN <Scorpion.TMN@ya.ru>
2024-01-21 09:01:05 +0700

    Мелкие исправления

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 import java.util.List;
 
 public class Main {
-    public static Box<Fruit> getFruitBox() {
+    public static void main(String[] args) {
+        Box<Fruit> fruitBox = getFruitBox();
+        printBox(fruitBox);
+    }
+    static Box<Fruit> getFruitBox() {
         Box<Fruit> fruitBox = new Box<>();
         fruitBox.put(new Apple());
         fruitBox.put(new Orange());
         return fruitBox;
     }
 
-    public static void printBox(Box<? extends Fruit> box) {
-        List<? extends Fruit> items = box.getItems();
-        for (Fruit fruit : items) {
+    static void printBox(Box<? extends Fruit> box) {
+//        List<? extends Fruit> items = box.getItems();
+        System.out.println("Содержимое коробки: ");
+        for (Fruit fruit : getFruitBox().getItems()) {
             System.out.println(fruit.toString());
         }
     }
 
-    public static void main(String[] args) {
-        Box<Fruit> fruitBox = getFruitBox();
-        printBox(fruitBox);
-    }
+
 }
